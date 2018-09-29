db3b6636d6c9e4ea93d4b821ce87791ad75d23a1
HuangJM7 <jm_william@qq.com>
2018-09-29 15:16:31 +0800

    0929

--- Baiduife/ZeroBasis/day44-46/extend.js
+++ Baiduife/ZeroBasis/day44-46/extend.js
@@ -3,18 +3,6 @@
 // 和原型链继承中直接使用subClass.prototype = new superClass()有什么区别呢，
 // 作为一项改进，它添加了一个空函数F，并将它创建的对象添加进原型链中，
 // 这样可以避免产生superClass的新实例
-// function extend(subClass, superClass) {
-//     var F = function () {};
-//     F.prototype = superClass.prototype;
-//     subClass.prototype = new F();
-//     subClass.prototype.constructor = subClass;
-//     subClass.superclass = superClass.prototype;
-//     //修正原型的constructor指向
-//     if (!superClass.prototype.contrucotor == Object.prototype.constructor) {
-//         superClass.prototype.constructor = superClass;
-//     }
-// }
-
 function extend(Sub, Sup) {
     var F = function () {};
     F.prototype = Sup.prototype;
@@ -25,4 +13,21 @@ function extend(Sub, Sup) {
         Sup.prototype.constructor = Sup;
     }
 
+}
+/*创建extend函数为了程序中所有的继承操作*/
+//subClass:子类  superClass：超类
+function extend(subClass,superClass) {
+    //1，使子类原型属性等于父类的原型属性
+    //初始化一个中间空对象，目的是为了转换主父关系
+    var F = function () {};
+    F.prototype = superClass.prototype;
+    //2， 让子类继承F
+    subClass.prototype = new F();
+    subClass.prototype.constructor = subClass;
+    //3，为子类增加属性 superClass ==》原型链的引用
+    subClass.superClass = superClass.prototype;
+    //4，增加一个保险，就算你的原型类是超类（Object）那么也要把你的构造函数级别降下来
+    if (superClass.prototype.constructor == Object.prototype.constructor) {
+        superClass.prototype.constructor = superClass;
+    }
 }
