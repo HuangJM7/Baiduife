e01b4a2ad14d06dc7329f1b862737c7e74847cb4
HuangJM7 <jm_william@qq.com>
2018-10-24 15:35:59 +0800

    Vue学习

---
+++ Vue demo/vue 2.5入门教程/vuecli/todolist/src/TodoList.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div>
+      <input type="text" v-model="inputValue">
+      <button @click="handleSumit">sumit</button>
+    </div>
+    <ul>
+      <todo-item  v-for="(item, index) in List" :key="index"  :index="index"
+      :content="item" @delete="handleDelete" >
+      </todo-item>
+
+    </ul>
+  </div>
+</template>
+
+<script>
+  import TodoItem from './components/TodoItem'
+  export default {
+    components: {
+      'todo-item': TodoItem
+    },
+    //es6 data(){return{ }}
+    data: function () {
+      return {
+        inputValue: '',
+        List: [],
+      }
+    },
+    methods: {
+      handleSumit() {
+        this.List.push(this.inputValue),
+          this.inputValue = ''
+      },
+      handleDelete(index){
+        this.List.splice(index,1)
+
+      }
+
+    }
+  }
+
+</script>
+
+<style>
+
+
+</style>
