afbdca932e956dca75c5d5757dfa6c289724c805
HuangJM7 <jm_william@qq.com>
2018-11-08 17:11:30 +0800

    home页面绘制

--- Vue demo/BlackHorseVue/day7/vue-cms/src/router.js
+++ Vue demo/BlackHorseVue/day7/vue-cms/src/router.js
@@ -1,10 +1,20 @@
 import VueRouter from 'vue-router'
 
+import HomeContainer from './component/tabbar/Homecontainer.vue'
+import MemberContainer from './component/tabbar/membercontainer.vue'
+import ShopcarContainer from './component/tabbar/Shopcarcontainer.vue'
+import SearchContainer from './component/tabbar/Searchcontainer.vue'
 
 // 3. 创建路由对象
 var router = new VueRouter({
   routes: [
-  ]
+    {path:'/',redirect:'/home'},
+    {path:'/home',component:HomeContainer},
+    {path:'/member',component:MemberContainer},
+    {path:'/shopcar',component:ShopcarContainer},
+    {path:'/search',component:SearchContainer},
+  ],
+  linkActiveClass:'mui-active'
 })
 
 // 把路由对象暴露出去
